493f1cc96bc031a68cb1480cdc9983b4137321d6
Luana Tonieti <luanawt43@gmail.com>
2023-09-16 15:25:21 -0300

    Pesquisar permitindo mais de uma seleção

--- SearchOptions.py
+++ SearchOptions.py
@@ -39,15 +39,15 @@ class SearchOptions(QMainWindow):
         else:
             if self.form.nomeRB.isChecked() and len(self.form.nomeText.text()) != 0 :
                 self.query += f" AND nome_projeto = '{self.form.nomeText.text()}'"
-            if self.form.idRB.isChecked():
+            if self.form.idRB.isChecked() and len(self.form.idText.text()) != 0:
                 self.query += f" AND codigo = {self.form.idText.text()}"
-            if self.form.alturaRB.isChecked():
+            if self.form.alturaRB.isChecked() and len(self.form.alturaText.text()) != 0:
                 self.query += f" AND altura {operations[self.form.alturaCB.currentText()]} {self.form.alturaText.text()}"
-            if self.form.larguraRB.isChecked():
+            if self.form.larguraRB.isChecked() and len(self.form.larguraText.text()) != 0:
                 self.query += f" AND largura {operations[self.form.larguraCB.currentText()]} {self.form.larguraText.text()}"
-            if self.form.comprimentoRB.isChecked():
+            if self.form.comprimentoRB.isChecked() and len(self.form.comprimentoText.text()) != 0:
                 self.query += f" AND comprimento {operations[self.form.comprimentoCB.currentText()]} {self.form.comprimentoText.text()}"
-            if self.form.volumeRB.isChecked():
+            if self.form.volumeRB.isChecked() and len(self.form.volumeText.text()) != 0:
                 self.query += f" AND altura*largura*comprimento {operations[self.form.volumeCB.currentText()]} {self.form.volumeText.text()}"
             if self.form.dataRB.isChecked():
                 self.query += f" AND data_imagem {operations[self.form.dataCB.currentText()]} '{self.form.data.date().toPyDate()}'"
